1c1e8e91cb8438cee122a1d38ab88918e9986d45
bduha184 <bduha_184@yahoo.co.jp>
2023-07-06 22:47:33 +0900

    トップページアイコン擬似要素調整

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -48,13 +48,16 @@ const menuLinks = computed(()=> {
         <v-col v-for="(item, index) in menuLinks" :key="index" cols="6" md="4">
           <v-card
           @click="item.func"
-          class="text-center"
+          class="text-center border w-60 h-60 m-auto d-flex flex-column justify-center align-center"
           >
               <v-icon
               :icon="item.icon"
+              class="relative before:absolute before:w-10 before:h-10 before:border-2 before:border-gray-400 before:rounded-full"
               >
               </v-icon>
-              <v-card-title>
+              <v-card-title
+              class="relative before:absolute before:w-2 before:h-10 before:bg-orange-600 before:left-0"
+              >
                 {{ item.text }}
               </v-card-title>
           </v-card>
@@ -64,3 +67,9 @@ const menuLinks = computed(()=> {
   </v-card>
 
 </template>
+
+<style scoped>
+.v-card--variant-elevated{
+  box-shadow: none;
+}
+</style>
